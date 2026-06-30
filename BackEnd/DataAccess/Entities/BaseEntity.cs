c9e9d8550770fb364aa8149fe45b8dc742533173
MlhDmn <mlhduman@hotmail.com>
2026-06-30 10:42:48 +0300

    Update project settings

--- BackEnd/DataAccess/Entities/BaseEntity.cs
+++ BackEnd/DataAccess/Entities/BaseEntity.cs
@@ -5,7 +5,7 @@ namespace B2B_Proje.DataAccess.Entities
         public int Id { get; set; } 
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow; 
         public bool IsActive { get; set; } = true;
-        public int? CreatedByUserId { get; set; }
+        public int CreatedByUserId { get; set; }
         public int? UpdatedByUserId { get; set; }
     }
 }
